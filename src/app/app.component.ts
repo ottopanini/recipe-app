a81f1aaa12b077fe9b90f6079581259ef149cf56
Christian Preß <Christian.Press@syracos.com>
2020-05-06 21:10:43 +0200

    Adding Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Module} from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+  selectedMenu = Module.RECIPES;
+
+  module = Module;
+
+  onShoppingChosen(module: Module) {
+    this.selectedMenu = module;
+  }
 }
