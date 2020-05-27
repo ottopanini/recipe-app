726cb2f72b04f3f5e496fc43aba5135ccceb07bb
cpress <cpress@it4ipm.de>
2020-05-27 22:01:46 +0200

    Adding Ingredient Controls to a Form Array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 import {RecipeService} from '../recipe.service';
 
 @Component({
@@ -12,6 +12,10 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+
+  get ingredientCtrls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
 
   constructor(private route: ActivatedRoute,
               private recipeService: RecipeService) { }
@@ -28,6 +32,7 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    const recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
@@ -35,6 +40,15 @@ export class RecipeEditComponent implements OnInit {
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+
+      if (recipe.ingredients) {
+        recipe.ingredients.forEach((ingredient) => recipeIngredients.push(new FormGroup({
+          // tslint:disable-next-line:object-literal-key-quotes
+          'name': new FormControl(ingredient.name),
+          // tslint:disable-next-line:object-literal-key-quotes
+          'amount': new FormControl(ingredient.amount)
+        })));
+      }
     }
 
     this.recipeForm = new FormGroup({
@@ -43,7 +57,9 @@ export class RecipeEditComponent implements OnInit {
       // tslint:disable-next-line:object-literal-key-quotes
       'imagePath': new FormControl(recipeImagePath),
       // tslint:disable-next-line:object-literal-key-quotes
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      // tslint:disable-next-line:object-literal-key-quotes
+      'ingredients': recipeIngredients
     });
   }
 
