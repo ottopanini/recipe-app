f04cd5d7fdd04bd6b63a9559270284e9e167ae40
cpress <cpress@it4ipm.de>
2020-05-30 21:11:53 +0200

    Storing Recipes

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Recipe} from '../recipes/recipe.model';
+import {RecipeService} from '../recipes/recipe.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+  private static readonly RECIPES_STORE = 'https://udang-recipe-book.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(DataStorageService.RECIPES_STORE, recipes).subscribe(response => {
+      console.log(response);
+    });
+  }
+}
