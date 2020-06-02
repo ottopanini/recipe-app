c59273bc4b37a90d3daf9f80d1586687f8890e29
cpress <cpress@it4ipm.de>
2020-06-02 23:10:28 +0200

    changes login mode default

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -11,7 +11,7 @@ import {Router} from '@angular/router';
 })
 export class AuthComponent implements OnInit {
   collapsed: boolean;
-  isLoginMode = false;
+  isLoginMode = true;
   isLoading = false;
   error: string = null;
 
