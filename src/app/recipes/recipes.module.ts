f2ac89c8520aec9708f7371e9a6cf4149e19a829
cpress <cpress@it4ipm.de>
2020-06-06 15:01:42 +0200

    Adding Routes to Feature Modules

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,10 +5,9 @@ import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
 import {RecipeStartComponent} from './recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
-import {RouterModule} from '@angular/router';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AppRoutingModule} from '../app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import {RecipesRoutingModule} from './recipes-Routing.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,7 @@ import {CommonModule} from '@angular/common';
     RecipeEditComponent,
   ],
   imports: [
-    AppRoutingModule,
+    RecipesRoutingModule,
     CommonModule,
     ReactiveFormsModule
   ],
